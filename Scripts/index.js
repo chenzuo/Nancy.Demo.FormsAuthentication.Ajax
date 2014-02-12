d4c8691739681d2b60e2fbe23529c745473a27e1
Nathan Chere <git@nathanchere.com.au>
2014-02-12 18:17:32 +1100

    Index.js

---
+++ Scripts/index.js
@@ -0,0 +1,19 @@
+﻿$('#loginForm').submit(tryLogon);
+
+function tryLogon(event) {
+    event.preventDefault();
+    var username = $('#Username').val();
+    var password = $('#Password').val();
+    $.ajax({
+        url: 'login',
+        type: "POST",
+        data: { UserName: username, Password: password }
+    })
+    .done(function (response) {
+        if (response.IsSuccess) {
+            $.cookie('_ncfa', response.AuthToken);
+        }
+        $('#StatusText').text(response.Message);
+    })
+    .fail(function (msg) { $('#StatusText').text('Error: ' + msg.statusText); });
+};
